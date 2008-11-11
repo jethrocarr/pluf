fe2351e157c1dff04de9def07eeea1ace5c03755
Loic d'Anterroches <titoo@users.sourceforge.net>
2008-11-11 09:23:28 +0100

    Improved the file upload functionnalities.
    - Added a new 'file_name' parameter to rename the uploaded file easily.
    - Renamed the moveToUploadFolder function to match Pluf conventions.

--- src/Pluf/Form/Field/File.php
+++ src/Pluf/Form/Field/File.php
@@ -24,7 +24,7 @@
 class Pluf_Form_Field_File extends Pluf_Form_Field
 {
     public $widget = 'Pluf_Form_Widget_FileInput';
-    public $move_function = 'Pluf_Form_Field_moveToUploadFolder';
+    public $move_function = 'Pluf_Form_Field_File_moveToUploadFolder';
     public $max_size = 2097152; // 2MB
     public $move_function_params = array();
 
@@ -81,7 +81,7 @@ class Pluf_Form_Field_File extends Pluf_Form_Field
 /**
  * Default move function. The file name is sanitized.
  *
- * In the extra parameters, 2 can be used so that this function is
+ * In the extra parameters, options can be used so that this function is
  * matching most of the needs:
  *
  *  * 'upload_path': The path in which the uploaded file will be
@@ -91,6 +91,13 @@ class Pluf_Form_Field_File extends Pluf_Form_Field
  *
  *  * 'upload_overwrite': Set it to true if you want to allow overwritting.
  *
+ *  * 'file_name': Force the file name to this name and do not use the
+ *                 original file name. If this name contains '%s' for
+ *                 example 'myid-%s', '%s' will be replaced by the
+ *                 original filename. This can be used when for
+ *                 example, you want to prefix with the id of an
+ *                 article all the files attached to this article.
+ *
  * If you combine those options, you can dynamically generate the path
  * name in your form (for example date base) and let this upload
  * function create it on demand.
@@ -99,7 +106,7 @@ class Pluf_Form_Field_File extends Pluf_Form_Field
  * @param array Extra parameters. If upload_path key is set, use it. (array())
  * @return string Name relative to the upload path.
  */
-function Pluf_Form_Field_moveToUploadFolder($value, $params=array())
+function Pluf_Form_Field_File_moveToUploadFolder($value, $params=array())
 {
     $name = Pluf_Utils::cleanFileName($value['name']);
     $upload_path = Pluf::f('upload_path', '/tmp');
@@ -109,6 +116,13 @@ function Pluf_Form_Field_moveToUploadFolder($value, $params=array())
     if (isset($params['upload_path_create']) and !is_dir($upload_path)) {
         if (false == @mkdir($upload_path, 0777, true)) {
             throw new Pluf_Form_Invalid(__('An error occured when creating the upload path. Please try to send the file again.'));
+        }
+    }
+    if (isset($params['file_name'])) {
+        if (false !== strpos($params['file_name'], '%s')) {
+            $name = sprintf($params['file_name'], $name);
+        } else {
+            $name = $params['file_name'];
         }
     }
     $dest = $upload_path.'/'.$name;
